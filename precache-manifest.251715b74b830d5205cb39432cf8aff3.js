b576cd6a9a8f5a74b61ee214c1d957c98f960c91
Valeriy Saphonov <xxrrrxx@yandex.ru>
2019-01-23 11:31:08 +0300

    Updates

---
+++ precache-manifest.251715b74b830d5205cb39432cf8aff3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f787507196bd5ea29501",
+    "url": "/CityBank-frontend-CRUD-app-task/static/js/runtime~main.f7875071.js"
+  },
+  {
+    "revision": "b1c615346f6b6f0979b5",
+    "url": "/CityBank-frontend-CRUD-app-task/static/js/main.b1c61534.chunk.js"
+  },
+  {
+    "revision": "595926d688538a917a97",
+    "url": "/CityBank-frontend-CRUD-app-task/static/js/1.595926d6.chunk.js"
+  },
+  {
+    "revision": "599beaf5ce7ae51ff4777bb986632aee",
+    "url": "/CityBank-frontend-CRUD-app-task/index.html"
+  }
+];
